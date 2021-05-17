76f479ba70fe367aa907ff0b9d6e51a1fb3161b6
LilyDeVille <lily.deville@hotmail.com>
2021-05-17 18:58:45 -0500

    Finished implenting add photo feature.

---
+++ src/main/java/org/launchcode/LiftOffProject/Repository/StepRepository.java
@@ -0,0 +1,11 @@
+package org.launchcode.LiftOffProject.Repository;
+
+
+import org.launchcode.LiftOffProject.models.Step;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StepRepository extends CrudRepository<Step, Integer> {
+
+
+
+}
